94522949f034b14bf8467d18d821fc68116fbed1
Calvin Dong <calvind777@gmail.com>
2016-06-26 14:31:35 -0700

    draws circle basic functionality

---
+++ draw.js
@@ -0,0 +1,9 @@
+function draw() {
+	var url = "http://www.calvindong.me"
+	
+	var shape = document.createElementNS(url, "circle");
+	shape.setAttributeNS(null, "cx", 25);
+	shape.setAttributeNS(null, "cy", 25);
+	shape.setAttributeNS(null, "r",  20);
+	shape.setAttributeNS(null, "fill", "green");
+}
